3fcb39ac9cfe24a819399d97953d8c0f4379c44e
Tanuj Mittal <tanuj@tanuj-Studio-1555.(none)>
2011-04-15 00:58:50 +0530

    created a modular structure

--- myshell.c
+++ myshell.c
@@ -23,25 +23,78 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_USER 32
+#define MAX_HOSTNAME 64
+#define MAX_CWD 256
+#define MAX_INPUT 1024
+
+char USER[MAX_USER], HOSTNAME[MAX_HOSTNAME], CWD[MAX_CWD];
+
 void init()
 {
 	printf("Welcome to myshell !\n\t* http://www.github.com/free2talk/myshell\n\n");
 }
 
-int main(int argc, char **argv)
+char* getShortCWD()
 {
-	init();
-	char hostname[50], command[100], CWD[200], CWD_SHORT[100];
-	gethostname(hostname, 50);
-	strcpy(CWD, getenv("PWD"));
-	strcpy(CWD_SHORT, strncpy(strstr(CWD,getenv("HOME")), "~", strlen(getenv("HOME"))));
-	printf("%s@%s:%s$ ", getenv("USER"), hostname, CWD_SHORT);
-	while(gets(command))
+	char *ptr = strstr(CWD, getenv("HOME"));
+	int i,j;
+	if(ptr == NULL || ptr != CWD)
+		return CWD;
+	else
 	{
-		if(strcmp(command, "exit")==0)
-			exit(1);
-		printf("%s@%s:%s$ ", getenv("USER"), hostname, CWD_SHORT);
+		ptr[0] = '~';
+		for(i=strlen(getenv("HOME")), j=1 ; i<=strlen(CWD) ; i++,j++)
+		{
+			ptr[j] = CWD[i];
+		}
+		return ptr;
 	}
-	return 0;
 }
 
+void printControl()
+{
+	char c;
+	if(strcmp(USER, "root") == 0)
+		c = '#';
+	else
+		c = '$';
+	printf("%s@%s:%s%c ", USER, HOSTNAME, getShortCWD(), c);
+}
+
+int main()
+{
+	char c[MAX_INPUT];
+	int i=0;
+	
+	//	Print welcome message
+	init();
+	
+	//	Start with endless loop of shell
+	while(1)
+	{
+		// Calculate stuff for control statement
+		strcpy(USER, getenv("USER"));
+		gethostname(HOSTNAME, MAX_HOSTNAME);
+		strcpy(CWD, getenv("PWD"));
+		
+		//	Print control statement
+		printControl();
+		
+		//	Get user input
+		while((c[i]=getchar())!=10)
+		{
+			i++;
+		}
+		c[i] = '\0';
+		
+		// input is 'exit'
+		if(strcmp(c, "exit") == 0)
+		{
+			exit(1);
+		}
+		
+		//	Reset input array
+		i = 0;
+	}
+}
