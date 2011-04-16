6cba1d25847ae07816cc39a9b7177133f19ed368
Tanuj Mittal <tanuj@tanuj-Studio-1555.(none)>
2011-04-17 00:43:54 +0530

    added execvp() and cd

--- myshell.c
+++ myshell.c
@@ -23,11 +23,14 @@
 #include <string.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_USER 32
 #define MAX_HOSTNAME 64
 #define MAX_CWD 256
 #define MAX_INPUT 1024
+#define MAX_ARGS 10
 
 char USER[MAX_USER], HOSTNAME[MAX_HOSTNAME], CWD[MAX_CWD];
 
@@ -60,7 +63,7 @@ void printControl()
 		c = '#';
 	else
 		c = '$';
-	printf("\n%s@%s:%s%c ", USER, HOSTNAME, getShortCWD(), c);
+	printf("<myshell>%s@%s:%s%c ", USER, HOSTNAME, getShortCWD(), c);
 }
 
 int getch() 
@@ -77,10 +80,29 @@ int getch()
 	return ch;
 }
 
+int prepareArgs(char *buffer, char *args[MAX_ARGS])
+{
+	char *temp = strtok(buffer, " ");
+	int i=0;
+	
+	while(temp != NULL)
+	{
+		args[i] = temp;
+		temp = strtok(NULL, " ");
+		i++;
+	}
+	args[i] = NULL;
+	
+	return i;
+}
+
 int main()
 {
 	char c[MAX_INPUT];
 	int i=0, flag=0;
+	char *args[MAX_ARGS];
+	pid_t pid;
+	int num_of_args;
 	
 	//	Print welcome message
 	init();
@@ -102,51 +124,65 @@ int main()
 			switch(c[i])
 			{
 				case   9 : // Tab pressed
-							break;
+						break;
 						  
 				case  10 : // Enter pressed
-							c[i] = '\0';
-							flag = 1;
-							break;
+						printf("\n");
+						c[i] = '\0';
+						flag = 1;
+						break;
 						  
 				case  27 : 
+						c[++i] = (char)getch();
+						if(c[i] == 91)
+						{
 							c[++i] = (char)getch();
-							if(c[i] == 91)
+							if(c[i] == 65)
 							{
-							  c[++i] = (char)getch();
-							  if(c[i] == 65)
-							  {
-								  // Up arrow key pressed
-								  printf("UA");
-							  }
-							  else if(c[i] == 66)
-							  {
-								  // Down arrow key pressed
-								  printf("DA");
-							  }
-							  else if(c[i] == 67)
-							  {
-								  // Right arrow key pressed
-								  printf("RA");
-							  }
-							  else if(c[i] == 68)
-							  {
-								  // Left arrow key pressed
-								  printf("LA");
-							  }
-							  else
-							  {
-								  // Any other special key pressed
-							  }
+							  // Up arrow key pressed
+							  printf("UA");
+							  i-=2;
 							}
-							break;
+							else if(c[i] == 66)
+							{
+							  // Down arrow key pressed
+							  printf("DA");
+							  i-=2;
+							}
+							else if(c[i] == 67)
+							{
+							  // Right arrow key pressed
+							  printf("RA");
+							  i-=2;
+							}
+							else if(c[i] == 68)
+							{
+							  // Left arrow key pressed
+							  printf("LA");
+							  i-=2;
+							}
+							else
+							{
+							  // Any other special key pressed
+							}
+						}
+						else
+						{
+							// Esc was pressed before this key
+							printf("%c", c[i]);
+							c[i-1] = c[i];
+						}
+						break;
 						  
 				case 127 : // Backspace
-							break;
+						printf("BK");
+						if(i > 0)
+							i--;
+						break;
 	  
 				default  : // Any other key pressed
-							printf("%c", c[i]);
-							i++;
+						printf("%c", c[i]);
+						i++;
 			}
 			if(flag)
 			{
@@ -155,10 +191,45 @@ int main()
 			}
 		}
 		
-		// input is 'exit'
-		if(strcmp(c, "exit") == 0)
+		// prepare *args[] for execvp()
+		num_of_args = prepareArgs(c, args);
+		
+		if(strcmp(args[0], "exit") == 0)
 		{
+			// input is 'exit'
 			exit(1);
+		}
+		else if(strcmp(args[0], "cd") == 0)
+		{
+			// input is 'cd'
+			if(strcmp(args[1], "..") == 0)
+			{
+				// Go one directory up
+			}
+			else
+			{
+				// set "PWD" to args[1]
+				setenv("PWD", args[1], 1);
+			}
+		}
+		else
+		{
+			pid = fork();
+			
+			if(pid < 0)
+			{
+				// fork() unsuccessful
+			}
+			else if(pid > 0)
+			{
+				// parent
+				wait(NULL);
+			}
+			else
+			{
+				// rewriting instruction set with c[]
+				execvp(c, args);
+			}
 		}
 		
 		//	Reset input array
